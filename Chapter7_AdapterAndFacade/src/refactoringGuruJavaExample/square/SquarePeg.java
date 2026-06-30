716f3312c46aacf2431152cf241ac1c1edad80fa
Daniel Kozak <kozak2321@gmail.com>
2026-06-30 14:23:54 +0200

    adapter

---
+++ Chapter7_AdapterAndFacade/src/refactoringGuruJavaExample/square/SquarePeg.java
@@ -0,0 +1,14 @@
+package refactoringGuruJavaExample.square;
+
+public class SquarePeg {
+    private double width;
+
+    public SquarePeg(double width) {
+        this.width = width;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+}
